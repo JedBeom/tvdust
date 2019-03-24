671345e32908ff88b5611e4876afe145df172c80
Bombwhale <solkblte@icloud.com>
2019-03-24 20:46:08 +0900

    Update handler.go

--- handler.go
+++ handler.go
@@ -1,38 +1,46 @@
 package main
 
 import (
-	"github.com/JedBeom/airq"
-	"github.com/kpango/glg"
 	"html/template"
 	"log"
 	"net/http"
+	"sync"
+
+	"github.com/JedBeom/airq"
+	"github.com/kpango/glg"
 )
 
 type Data struct {
 	Grade string
-	Bg string
+	Bg    string
 }
 
 func index() http.HandlerFunc {
 	var (
-		// init sync.Once
-		t *template.Template
+		init sync.Once
+		t    *template.Template
 	)
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		/*init.Do(func(){
+		init.Do(func() {
 			t = template.Must(template.ParseFiles("tmpl/main.html"))
 		})
-		*/
-
-		t = template.Must(template.ParseFiles("tmpl/main.html"))
 
 		q, err := airq.NowByStation("연향동")
 		if err != nil {
-			_ = glg.Error(err)
-		}
+			_ = glg.Error(err, "station: 연향동")
 
+			q, err = airq.NowByStation("장천")
+			if err != nil {
+				_ = glg.Error(err, "station: 장천동")
+
+				err = t.Execute(w, Data{Grade: "문제발생!", Bg: "#FD99E1"})
+				return
+			}
+
+		}
 		err = t.Execute(w, setGrade(q.Pm10GradeWHO, q.Pm25GradeWHO))
+
 		if err != nil {
 			log.Println(err)
 		}
@@ -40,4 +48,3 @@ func index() http.HandlerFunc {
 		_ = glg.Info("Pm10:", q.Pm10GradeWHO, "Pm25:", q.Pm25GradeWHO)
 	}
 }
-
